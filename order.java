2da0bc9c3da9eee996a5378f12e24beb2e482a95
Mariam22-kamal <60687586+Mariam22-kamal@users.noreply.github.com>
2022-06-03 12:52:46 +0200

    Update order.java

--- order.java
+++ order.java
@@ -7,18 +7,17 @@ package OOP_PRJ;
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
-*/
+ */
 import java.util.*;
-//import java.util.HashMap;
-//import  java.util.ArrayList;
 /**
  *
  * @author lenovo
  */
 public class order {
+  
     HashMap<items,Integer> purchased_list = new HashMap<>();
     ArrayList<items> item_list = new ArrayList<>();
-    
+    double total_order_price;
     public order(items p, int quantity, int size){
         
          for(int i=0;i<size;i++)
@@ -33,23 +32,19 @@ public class order {
    }
     public double calprice(int size)
     {
-        double total_order_price = 0;
-        //items i=new items();
         purchased_list.forEach((key, Value) -> {
-            
-            total_order_price=(key.selling_price*Value)+total_order_price;
-            
+            total_order_price+=(key.selling_price*Value);   
         });
-        //for( items i : purchased_list)
-        {//iterator
-           //purchased_list.values(i));
-           
-           //System.out.println("key: " + i + " value: " + purchased_list.get((i)));//values(i));
-//purchased_list.getValue(i);
-        }
-        return 887;
+        
+        return total_order_price;
+        
     }
         
    
     
 }
+   
+    
+    
+   
+    
