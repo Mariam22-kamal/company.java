48248bf479b32e33abfa4586208e28f0d32620a2
Mariam22-kamal <60687586+Mariam22-kamal@users.noreply.github.com>
2022-06-03 18:34:56 +0200

    Update Balance_Sheet.java

--- Balance_Sheet.java
+++ Balance_Sheet.java
@@ -1,51 +1,41 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
-package OOP_PRJ;
+package javaapplication3;
+
 
 import java.util.ArrayList;
-import java.util.HashMap;
+
 
 /**
  *
  * @author Omar
  */
-public final class Balance_Sheet {
+public final class balance_sheet {
 
-double Total_Manfucture_Cost; // cost_item+cost_of_raw_material
 double Total_Revenue;//no_item_sold*selling_price
 double Net_Profit;//rev-manufacosr
    public double Cal_total_cost(ArrayList<customer> Customers_List)
    {
-       customer iterator=new customer();
+       customer iterator = new customer();
        
        int item_quantity=0;
        int No_Of_Customers=Customers_List.size();
-       int No_Of_Orders=0;
-       int No_Of_Items=0;
+       int No_Of_Orders=iterator.list.size();
        double Total_Cost=0;
        for(int i=0;i<No_Of_Customers;i++)                                       //for loop to run through each customer
        {
            iterator=Customers_List.get(i);
            for(int j=0;i<No_Of_Orders;i++)                                      //for loop to run through orders in the each customer  
            {
-               iterator.list.get(j);
-               for (int k=0;k<No_Of_Items;k++)                                  //for loop to run through items in the order
-               {
-//                   Total_Cost+=iterator.list.get(j).purchased_list.get(k).cost*item_quantity;
+                  Total_Cost+=iterator.list.get(j).total_order_cost;
+                  Total_Revenue+=iterator.list.get(j).total_order_price;
                
-               
-               
-               }
-           
-           }
-           
-       
-               
+           }  
        }
-       
-       
-       return Total_Cost;
+        Net_Profit=Total_Revenue-Total_Cost;
+       return Net_Profit;
    }
 }
